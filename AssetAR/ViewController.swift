8b873b0ce98f4fea8f1934eea64f5fb71c8f4796
Jules Gribble <gribblej@me.com>
2017-10-11 17:57:23 +0100

    added heading to AR world with sample code

--- AssetAR/ViewController.swift
+++ AssetAR/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import SceneKit
 import ARKit
+import CoreLocation
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
+class ViewController: UIViewController, ARSCNViewDelegate,CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    var objectSet = false
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -28,6 +30,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        } else{
+            print("location manager disabled")
+        }
+        //51.441431, -0.941817
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if(!objectSet){
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        print("location data -> \(String(describing: manager.location))")
+        let ballShape = SCNSphere(radius: 0.09)
+      //  let material = SCNMaterial()
+       // material.diffuse.contents = UIImage(named: "art.scnassets/sun.jpg")
+       // ballShape.materials = [material]
+        let ballNode = SCNNode(geometry: ballShape)
+         //   ballNode.position = SCNVector3Make(1.329,0.3,0)
+            //work out the location relative to the point the device is
+            ballNode.position = SCNVector3Make(Float(locValue.latitude) - (51.441431),0, Float(locValue.longitude) - (-0.941817))
+        sceneView.scene.rootNode.addChildNode(ballNode)
+            objectSet = true
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +66,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        //detect planes and set the heading of the ARKit world to the heading of north
+        configuration.planeDetection = .horizontal
+        configuration.worldAlignment = .gravityAndHeading
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
--- LogBook.md
+++ LogBook.md
@@ -4,4 +4,4 @@
 - Research existing apps using ARKit and benifits of these apps.
 ## Week 2
 - Map ARKit example – (use this as a base of what the overlay should look like) [YouTube](https://www.youtube.com/watch?v=6Lo0Z7CkMWw) & [GitHub](https://github.com/ProjectDent/ARKit-CoreLocation/blob/master/ARKit%2BCoreLocation/ViewController.swift)
-
+- Created a simple class for assets in swift to enable access of the data relating to one record. 
