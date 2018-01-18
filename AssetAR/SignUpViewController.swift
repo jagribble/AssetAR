4ee8f6ab501143a1f2452bce80bb991f66bf98ce
Jules Gribble <gribblej@me.com>
2018-01-18 01:08:51 +0000

    fixed sign up slow load view controller

--- AssetAR/SignUpViewController.swift
+++ AssetAR/SignUpViewController.swift
@@ -22,31 +22,52 @@ class SignUpViewController:UIViewController{
     
     @IBAction func signUp(_ sender: Any) {
         let sv = UIViewController.displaySpinner(onView: self.view)
-        Auth0
-            .authentication()
-            .createUser(
-                email: "\(email.text!)",
-                password: "\(password.text!)",
-                connection: "Username-Password-Authentication",
-                userMetadata: ["first_name": "\(firstName.text!)",
-                               "last_name": "\(lastName.text!)"]
-            )
-            .start { result in
-                switch result {
-                case .success(let user):
-                    print("User Signed up: \(user)")
-                    let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login") as UIViewController
-                    self.present(viewController, animated: true, completion: {
-                        UIViewController.removeSpinner(spinner: sv)
-                    })
-                
-                case .failure(let error):
-                    print("Failed with \(error)")
-                    UIViewController.removeSpinner(spinner: sv)
-                    let alert = UIAlertController(title: "Alert", message: "\(error)", preferredStyle: UIAlertControllerStyle.alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-                    self.present(alert, animated: true, completion: nil)
-                }
+        let group = DispatchGroup()
+        var errorString = ""
+        var status = false
+        let _email = email.text!
+        let _password = password.text!
+        let _firstName = firstName.text!
+        let _lastName = lastName.text!
+        group.enter()
+         DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async{
+            Auth0
+                .authentication()
+                .createUser(
+                    email: "\(_email)",
+                    password: "\(_password)",
+                    connection: "Username-Password-Authentication",
+                    userMetadata: ["first_name": "\(_firstName)",
+                                   "last_name": "\(_lastName)"]
+                )
+                .start { result in
+                    switch result {
+                    case .success(let user):
+                        print("User Signed up: \(user)")
+                        status = true
+                        group.leave()
+                   
+                    
+                    case .failure(let error):
+                        print("Failed with \(error)")
+                        errorString = error as! String
+                        status = false
+                        group.leave()
+                      
+                    }
+            }
+        }
+        group.wait()
+        if(status){
+            let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login") as UIViewController
+            self.present(viewController, animated: true, completion: {
+                UIViewController.removeSpinner(spinner: sv)
+            })
+        } else{
+            UIViewController.removeSpinner(spinner: sv)
+            let alert = UIAlertController(title: "Alert", message: "\(errorString)", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
     }
     
